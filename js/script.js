c960f52093928e047b27ec390436b574d86bdcde
Polina Fedorova <fedorovapolina94@gmail.com>
2017-02-22 04:08:23 +0200

    snowboards slider almost awesome!

--- js/script.js
+++ js/script.js
@@ -1,14 +1,15 @@
+/*popup*/
 $(".features__item-photos").on("click",function () {
     var wrap=$(this).parent(".features__item-photos-wr");
     var wrclone= wrap.clone();
-
 
         wrclone.insertAfter(".mainwrapper");
         $(wrclone[0]).attr("class","popup clone");
         $(".mainwrapper").attr("class","mainwrapper blur");
         $(wrclone[0]).children(".features__item-photos").eq(0).attr("class","popup-img");
+
+
     $(".popup-img").on("click",function () {
-        console.log(this);
         $(".clone").detach();
         $(".blur").attr("class","mainwrapper");
     });
